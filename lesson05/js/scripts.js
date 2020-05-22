4c04e71ec5014acdf9a75d89b262c490752235a6
craiglarson <craig.larson87@gmail.com>
2020-05-21 21:58:13 -0600

    css js changes

--- lesson05/js/scripts.js
+++ lesson05/js/scripts.js
@@ -5,4 +5,15 @@ function toggleMenu() {
 
 let d = new Date();
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//Get the today's date for the bottom of the page
 document.getElementById("currentDate").innerHTML = months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+
+//Get the day to display the pancakes in the park
+let day = d.getDay();
+if(day == 5) {
+   document.getElementById("pancakes").style.display = "block";
+}
+
+else {
+   document.getElementById("pancakes").style.display = "none";
+}
