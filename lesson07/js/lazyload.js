4e2970d325e70e227030147a1ef98d11e9af6343
craiglarson <craig.larson87@gmail.com>
2020-06-02 22:11:27 -0600

    Lazy Upload JS

---
+++ lesson07/js/lazyload.js
@@ -0,0 +1,35 @@
+//get all imgs with data-src attributes
+const imagesToLoad = document.querySelectorAll("img[data-src");
+
+//optional parameters being set for the IntersectionalObserver
+const imageOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+
+//first check to see if the Intersection Observer is supported
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imageOptions);
+
+    //Loop through each img and check status and load if necessary
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });    
+}
+else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
