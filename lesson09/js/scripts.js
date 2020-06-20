6949a1204a7b3cb6967632df8223630aa3c05430
craiglarson <craig.larson87@gmail.com>
2020-06-19 22:20:03 -0600

    css changes

--- lesson09/js/scripts.js
+++ lesson09/js/scripts.js
@@ -9,4 +9,4 @@ let day =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sat
 //Get the today's date for the bottom of the page
 document.getElementById("currentDate").innerHTML = day[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
 
-
+WebFont.load({google: {families: ["Quicksand", "Righteous"]}});
