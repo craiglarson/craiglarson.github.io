366413ab3f739d5eae78677b810d777b9c7b1f10
craiglarson <craig.larson87@gmail.com>
2020-06-29 12:12:30 -0600

    css changes

---
+++ lesson11/js/towns.js
@@ -0,0 +1,40 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+      return response.json();
+  })
+  .then(function (jsonObject) {
+      const towns = jsonObject["towns"];
+      console.table(jsonObject); //temporary checking for valid response and data parsing
+      for (let i = 0; i < towns.length; i++) {
+          if(towns[i].name == "Preston" || towns[i].name == "Soda Springs" || towns[i].name == "Fish Haven") {
+              let card = document.createElement('section');
+              let cardCopy = document.createElement('div')
+              cardCopy.setAttribute('class', 'card-copy');
+              let name = document.createElement('h2');
+              let motto = document.createElement('h5');
+              let founding = document.createElement('p');
+              let population = document.createElement('p');
+              let precipitation = document.createElement('p');
+              let image = document.createElement('img');              
+
+              name.textContent = towns[i].name;
+              motto.textContent = towns[i].motto;
+              founding.textContent = "Year Founded: " + towns[i].yearFounded;
+              population.textContent = "Population: " + towns[i].currentPopulation;
+              precipitation.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
+              image.setAttribute('src', "images/" + towns[i].photo);              
+
+              card.appendChild(cardCopy);
+              cardCopy.appendChild(name);
+              cardCopy.appendChild(motto);
+              cardCopy.appendChild(founding);
+              cardCopy.appendChild(population);
+              cardCopy.appendChild(precipitation);
+              card.appendChild(image);              
+
+              document.querySelector('div.cards').appendChild(card);
+          }
+      }
+  })
