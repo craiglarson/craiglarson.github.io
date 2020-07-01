aaba1b1595c12656a5bb0aa08df36a1666ff62f8
craiglarson <craig.larson87@gmail.com>
2020-06-30 22:59:26 -0600

    Map Adjustments

--- lesson11/js/towns.js
+++ lesson11/js/towns.js
@@ -24,7 +24,7 @@ fetch(requestURL)
               founding.textContent = "Year Founded: " + towns[i].yearFounded;
               population.textContent = "Population: " + towns[i].currentPopulation;
               precipitation.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
-              image.setAttribute('src', "images/" + towns[i].photo);
+              image.setAttribute('src', `images/homepage/${towns[i].photo}`);
               image.setAttribute('alt', `Picture of ${towns[i].name}`);              
 
               card.appendChild(cardCopy);
