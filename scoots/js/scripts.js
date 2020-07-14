195c6eb846e5c119968fb95d4aff366f4a3f7e0a
craiglarson <craig.larson87@gmail.com>
2020-07-13 22:35:18 -0600

    Menu adjustments

--- scoots/js/scripts.js
+++ scoots/js/scripts.js
@@ -1,3 +1,6 @@
 function toggleMenu() {
     document.getElementById('primaryNav').classList.toggle('hide');
 }
+
+//GOOGLE FONTS
+WebFont.load({google: {families: ["Seaweed Script", "Architects Daughter", "Roboto"]}});
